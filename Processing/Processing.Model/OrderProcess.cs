f770bd10a52bddaead55b059827a43d209bb4d6e
Aravinthan-18 <aravinthan1606@gmail.com>
2023-06-02 12:32:37 +0530

    LIST

---
+++ Processing/Processing.Model/OrderProcess.cs
@@ -0,0 +1,13 @@
+﻿namespace Processing.Model
+{
+    public class OrderProcess
+    {
+        public int Number { get; set; }
+        public int OrderID { get; set; }
+        public string Name  { get; set; }
+        public int Unit { get; set; }
+        public int Quantaty { get; set; }
+        public int Total { get;set; }
+
+    }
+}
--- Processing/Processing/MainWindow.xaml.cs
+++ Processing/Processing/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Processing.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,26 @@ namespace Processing
         public MainWindow()
         {
             InitializeComponent();
+            List<OrderProcess> processs = new List<OrderProcess>()
+            {
+                new OrderProcess {Number=1,OrderID=1,Name="BAT",Unit=200,Quantaty=2,Total=500},
+                 new OrderProcess {Number=2,OrderID=2,Name="BALL",Unit=300,Quantaty=3,Total=600},
+                  new OrderProcess {Number=3,OrderID=3,Name="KIT",Unit=400,Quantaty=4,Total=700}
+            };
+            lstname.ItemsSource = processs;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<OrderProcess> processs = new List<OrderProcess>()
+            {
+                new OrderProcess {Number=1,OrderID=1,Name="BAT",Unit=200,Quantaty=2,Total=500},
+                 new OrderProcess {Number=2,OrderID=2,Name="BALL",Unit=300,Quantaty=3,Total=600},
+                  new OrderProcess {Number=3,OrderID=3,Name="KIT",Unit=400,Quantaty=4,Total=700}
+            };
+            lstname.ItemsSource=processs;
+
         }
     }
 }
